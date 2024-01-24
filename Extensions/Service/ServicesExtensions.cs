b6040a1b9f75c31f9c7d2920608ccb992cb46cef
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-01-23 21:53:52 -0500

    DA-4-Added scoped service now api client

--- Extensions/Service/ServicesExtensions.cs
+++ Extensions/Service/ServicesExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using DispoDataAssistant.Data.Models;
 using DispoDataAssistant.Factories;
+using DispoDataAssistant.Interfaces;
 using DispoDataAssistant.Managers.Implementations;
 using DispoDataAssistant.Managers.Interfaces;
+using DispoDataAssistant.Services;
 using DispoDataAssistant.Services.Implementations;
 using DispoDataAssistant.Services.Interfaces;
 using DispoDataAssistant.UIComponents;
@@ -10,6 +12,8 @@ using DispoDataAssistant.UIComponents.Main;
 using DispoDataAssistant.UIComponents.Settings;
 using DispoDataAssistant.UIComponents.ViewPane;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Configuration;
 
 namespace DispoDataAssistant.Extensions.Service
 {
@@ -23,6 +27,24 @@ namespace DispoDataAssistant.Extensions.Service
             services.AddSingleton<MainViewModel>();
             services.AddSingleton<TabControlEditViewModel>();
             services.AddSingleton<TabControlButtonsViewModel>();
+        }
+
+        public static void AddClients(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceNowApiClient>((provider) =>
+            {
+                var baseUrl = ConfigurationManager.AppSettings["ServiceNowBaseUrl"];
+                var logger = provider.GetRequiredService<ILogger<ServiceNowApiClient>>();
+
+                if (baseUrl is null)
+                {
+                    return new ServiceNowApiClient("https://ummeddev.service-now.com/api/now/", logger);
+                }
+                else
+                {
+                    return new ServiceNowApiClient(baseUrl, logger);
+                }
+            });
         }
 
         public static void AddInternalServices(this IServiceCollection services)
@@ -41,7 +63,6 @@ namespace DispoDataAssistant.Extensions.Service
 
             //Factories
             services.AddSingleton<TabItemFactory>();
-
         }
 
         public static void AddViews(this IServiceCollection services)
