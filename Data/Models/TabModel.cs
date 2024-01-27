0f10e9cc730b5d5c11e04f581b1239ca8db00b96
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-01-27 17:43:52 -0500

    DA-5-Added tab index property

--- Data/Models/TabModel.cs
+++ Data/Models/TabModel.cs
@@ -7,6 +7,7 @@ namespace DispoDataAssistant.Data.Models
     public partial class TabModel : ObservableObject
     {
         public int Id { get; set; }
+        public int? Index { get; set; } = null;
         [ObservableProperty]
         private string name = string.Empty;
         public virtual ObservableCollection<ServiceNowAsset> ServiceNowAssets { get; set; } = new ObservableCollection<ServiceNowAsset>();
