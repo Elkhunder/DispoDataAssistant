ea63d60437cd071426a7b2a58b8921edb4d3fa73
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-01-27 17:43:25 -0500

    DA-5-Added tab index migration

---
+++ Migrations/Asset/20240127221149_AddTabIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DispoDataAssistant.Migrations.Asset
+{
+    /// <inheritdoc />
+    public partial class AddTabIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "Tabs",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "Tabs");
+        }
+    }
+}
