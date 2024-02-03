34eadad848232493beca6cf778beee3710a7b13f
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-02-02 20:36:44 -0500

    DA-20-Added lifecycle member models

---
+++ Data/Models/ServiceNow/LifecycleItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DispoDataAssistant.Data.Models.ServiceNow
+{
+    public class LifecycleItem
+    {
+        [JsonPropertyName("sys_id")]
+        public string? SysId { get; set; }
+        [JsonPropertyName("sys_updated_by")]
+        public string? UpdatedBy { get; set; }
+        [JsonPropertyName("sys_created_on")]
+        public string? CreatedOn { get; set; }
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("sys_mod_count")]
+        public string? ModCount { get; set; }
+        [JsonPropertyName("sys_updated_on")]
+        public string? UpdatedOn { get; set; }
+        [JsonPropertyName("sys_tags")]
+        public string? Tags { get; set; }
+        [JsonPropertyName("sys_created_by")]
+        public string? CreatedBy { get; set; }
+    }
+}
---
+++ Data/Models/ServiceNow/LifecycleStatusesApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace DispoDataAssistant.Data.Models.ServiceNow
+{
+    public class LifecycleStatusesApiResponse
+    {
+        [JsonPropertyName("result")]
+        public List<LifecycleItem>? LifecycleStatuses { get; set; }
+    }
+}
