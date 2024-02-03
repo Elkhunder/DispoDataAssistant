4b5d8116193b424682171d6079a237e993160670
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-02-02 20:30:53 -0500

    DA-20-Added method to get lifecycle stage and status members

--- Services/Interfaces/IServiceNowApiClient.cs
+++ Services/Interfaces/IServiceNowApiClient.cs
@@ -8,5 +8,8 @@ public interface IServiceNowApiClient
 {
     Task<RestResponse<ServiceNowApiResponse>> GetServiceNowAssetByAssetTagAsync(string assetTag);
     Task<RestResponse<ServiceNowApiResponse>> GetServiceNowAssetBySerialNumberAsync(string serialNumber);
-    Task<RestResponse<ServiceNowApiResponse>> RetireServiceNowAssetAsync(string sys_id);
+    Task<RestResponse<ServiceNowApiResponse>> GetServiceNowAssetsByIdAsync(string[] sys_ids);
+    Task<RestResponse<ServiceNowApiResponse>> GetServiceNowAssetByIdAsync(string sys_id);
+    Task<RestResponse<ServiceNowApiResponse>> RetireServiceNowAssetAsync(string sys_id, object payload);
+    Task<LifecycleMembers> GetLifecycleMembersAsync();
 }
