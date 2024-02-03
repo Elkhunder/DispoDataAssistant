315c3a6e9400869210d57f75191fbe8d20216119
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-02-03 15:35:02 -0500

    DA-20-Added query parameter

--- Services/Implementations/ServiceNowApiClient.cs
+++ Services/Implementations/ServiceNowApiClient.cs
@@ -93,8 +93,9 @@ public class ServiceNowApiClient : BaseService, IServiceNowApiClient
 
     public async Task<RestResponse<ServiceNowApiResponse>> RetireServiceNowAssetAsync(string sys_id, object payload)
     {
-        var request = new RestRequest($"table/alm_hardware/{sys_id}");
-        request.AddJsonBody(payload);
+        var request = new RestRequest($"table/alm_hardware/{sys_id}")
+            .AddQueryParameter("sysparm_fields", "sys_id,parent,asset_tag,model.name,model_category.name,sys_updated_on,substatus,install_status,life_cycle_stage.name,life_cycle_stage_status.name,serial_number,model.manufacturer.name")
+            .AddJsonBody(payload);
 
         return await _client.ExecutePutAsync<ServiceNowApiResponse>(request);
     }
