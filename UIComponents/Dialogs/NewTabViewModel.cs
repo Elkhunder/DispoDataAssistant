75c49d6df10b173b70d943793aeb66b823518c60
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-02-11 14:40:43 -0500

    DA-25-Added classes for tab name input dialog

---
+++ UIComponents/Dialogs/NewTabViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DispoDataAssistant.UIComponents.Dialogs
+{
+    public partial class NewTabViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool isTabNameToggled = false;
+        [ObservableProperty]
+        private string title = "Create a New Tab?";
+        [ObservableProperty]
+        private Visibility toggleVisibility = Visibility.Visible;
+        [ObservableProperty]
+        private string name;
+    }
+}
